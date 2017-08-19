71cf1621c85a839120444ea771d24a4b3d13b951
Kas Perch <the@nodebotani.st>
2017-08-19 14:20:34 +1000

    Cleaned up serial lib

--- lib/serial.c
+++ lib/serial.c
@@ -1,96 +1,70 @@
-
-/*
-  Quick and dirty functions that make serial communications work.
-
-  Note that receiveByte() blocks -- it sits and waits _forever_ for
-   a byte to come in.  If you're doing anything that's more interesting,
-   you'll want to implement this with interrupts.
-
-   initUSART requires BAUDRATE to be defined in order to calculate
-     the bit-rate multiplier.  9600 is a reasonable default.
-
-  May not work with some of the older chips:
-    Tiny2313, Mega8, Mega16, Mega32 have different pin macros
-    If you're using these chips, see (e.g.) iom8.h for how it's done.
-    These old chips don't specify UDR0 vs UDR1.
-    Correspondingly, the macros will just be defined as UDR.
-*/
-
 #include <avr/io.h>
 #include "serial.h"
 #include <util/setbaud.h>
 
-void initSerial(void) {                                /* requires BAUD */
-  UBRR0H = UBRRH_VALUE;                        /* defined in setbaud.h */
+void initSerial(void) {
+  UBRR0H = UBRRH_VALUE;
   UBRR0L = UBRRL_VALUE;
 #if USE_2X
   UCSR0A |= (1 << U2X0);
 #else
   UCSR0A &= ~(1 << U2X0);
 #endif
-                                  /* Enable USART transmitter/receiver */
   UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);   /* 8 data bits, 1 stop bit */
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
 
-
 void transmitByte(uint8_t data) {
-                                     /* Wait for empty transmit buffer */
   loop_until_bit_is_set(UCSR0A, UDRE0);
-  UDR0 = data;                                            /* send data */
+  UDR0 = data;
 }
 
 uint8_t receiveByte(void) {
-  loop_until_bit_is_set(UCSR0A, RXC0);       /* Wait for incoming data */
-  return UDR0;                                /* return register value */
+  loop_until_bit_is_set(UCSR0A, RXC0);
+  return UDR0;
 }
 
-
-                       /* Here are a bunch of useful printing commands */
-
-void printString(const char myString[]) {
+void printString(const char string[]) {
   uint8_t i = 0;
-  while (myString[i]) {
-    transmitByte(myString[i]);
+  while (string[i]) {
+    transmitByte(string[i]);
     i++;
   }
 }
 
-void readString(char myString[], uint8_t maxLength) {
+void readString(char string[], uint8_t maxLength) {
   char response;
   uint8_t i;
   i = 0;
-  while (i < (maxLength - 1)) {                   /* prevent over-runs */
+  while (i < (maxLength - 1)) {
     response = receiveByte();
-    transmitByte(response);                                    /* echo */
-    if (response == '\r') {                     /* enter marks the end */
+    transmitByte(response);
+    if (response == '\r') {
       break;
     }
     else {
-      myString[i] = response;                       /* add in a letter */
+      string[i] = response;
       i++;
     }
   }
-  myString[i] = 0;                          /* terminal NULL character */
+  string[i] = 0;
 }
 
 void printByte(uint8_t byte) {
-              /* Converts a byte to a string of decimal text, sends it */
-  transmitByte('0' + (byte / 100));                        /* Hundreds */
-  transmitByte('0' + ((byte / 10) % 10));                      /* Tens */
-  transmitByte('0' + (byte % 10));                             /* Ones */
+  transmitByte('0' + (byte / 100));
+  transmitByte('0' + ((byte / 10) % 10));
+  transmitByte('0' + (byte % 10));
 }
 
 void printWord(uint16_t word) {
-  transmitByte('0' + (word / 10000));                 /* Ten-thousands */
-  transmitByte('0' + ((word / 1000) % 10));               /* Thousands */
-  transmitByte('0' + ((word / 100) % 10));                 /* Hundreds */
-  transmitByte('0' + ((word / 10) % 10));                      /* Tens */
-  transmitByte('0' + (word % 10));                             /* Ones */
+  transmitByte('0' + (word / 10000));
+  transmitByte('0' + ((word / 1000) % 10));
+  transmitByte('0' + ((word / 100) % 10));
+  transmitByte('0' + ((word / 10) % 10));
+  transmitByte('0' + (word % 10));
 }
 
 void printBinaryByte(uint8_t byte) {
-                       /* Prints out a byte as a series of 1's and 0's */
   uint8_t bit;
   for (bit = 7; bit < 255; bit--) {
     if (bit_is_set(byte, bit))
@@ -100,18 +74,7 @@ void printBinaryByte(uint8_t byte) {
   }
 }
 
-char nibbleToHexCharacter(uint8_t nibble) {
-                                   /* Converts 4 bits into hexadecimal */
-  if (nibble < 10) {
-    return ('0' + nibble);
-  }
-  else {
-    return ('A' + nibble - 10);
-  }
-}
-
 void printHexByte(uint8_t byte) {
-                        /* Prints a byte as its hexadecimal equivalent */
   uint8_t nibble;
   nibble = (byte & 0b11110000) >> 4;
   transmitByte(nibbleToHexCharacter(nibble));
@@ -120,18 +83,15 @@ void printHexByte(uint8_t byte) {
 }
 
 uint8_t getNumber(void) {
-  // Gets a numerical 0-255 from the serial port.
-  // Converts from string to number.
   char hundreds = '0';
   char tens = '0';
   char ones = '0';
   char thisChar = '0';
-  do {                                                   /* shift over */
+  do {
     hundreds = tens;
     tens = ones;
     ones = thisChar;
-    thisChar = receiveByte();                   /* get a new character */
-    transmitByte(thisChar);                                    /* echo */
-  } while (thisChar != '\r');                     /* until type return */
+    thisChar = receiveByte();
+  } while (thisChar != '\r');
   return (100 * (hundreds - '0') + 10 * (tens - '0') + ones - '0');
 }
